92f5a1cb20cd6e9dd75c6c52c6b265108738f590
snipes <schneidsmc@gmail.com>
2024-01-08 14:17:27 -0600

    README and comments in JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,8 @@ var fiveDay = document.querySelector('.five-day');
 var currentContainer = document.querySelector('.currentContainer')
 var clearBtn = document.querySelector('.clear');
 
+
+// Initial function beginning with the click on the search button.
 searchEl.addEventListener('click', btn);
 
 function btn (event) {
@@ -12,12 +14,13 @@ function btn (event) {
     var searchInputVal = document.querySelector('#myInput').value;
     
 console.log(searchInputVal)
+// Unrecognizable input will alert user to try again.
 if (!searchInputVal) {
   alert('Unrecognizable Input');
         console.error('You need a search input value!');
         return;
     }
-
+// Search city will be stored in the Local Storage to be displayed as a search history.
     localStorage.setItem('searchInputVal', searchInputVal);
 
     var previousInputs = JSON.parse(localStorage.getItem('searchInputs')) || [];
@@ -30,6 +33,7 @@ if (!searchInputVal) {
       previousInputs.shift(); 
   }
 
+  // Runs the function to load the Search History when the page loads so the user can click one instead of typing.
   loadSearchHistory();
   searchApi(searchInputVal);
 
@@ -58,6 +62,7 @@ if (!searchInputVal) {
         var lat = searchLoc.lat;
         var lon = searchLoc.lon;
 
+        // API using the lat/long to find the corresponding city.
         var tempURL = 'https://api.openweathermap.org/data/2.5/forecast'
 
         var tempURL = tempURL + '?lat=' + lat + '&lon=' + lon +'&units=imperial&appid=e54dee0cc53d0b5d7fada68322d11e01'
@@ -72,6 +77,7 @@ if (!searchInputVal) {
   
         return response.json();
       })
+      // Testing the response of the API.
       .then(function (weatherData) {
         console.log(weatherData);
         console.log("City:", weatherData.city.name);
@@ -80,7 +86,7 @@ if (!searchInputVal) {
         console.log("Wind:", weatherData.list[0].wind.speed);
         console.log(weatherData.list[0].weather[0].icon)
       
-        
+      // Functions called to display the current weather and the 5-day forecast.
         printResults(weatherData);
 
         fiveResults(weatherData);
@@ -92,12 +98,12 @@ if (!searchInputVal) {
       });
     }
     
-
+// Current Weather function.
 function printResults(weatherData) {
   var currentContainer = document.querySelector('.currentContainer');
   currentContainer.innerHTML = ''; 
 
-
+// Variables designated from the API response.
 var temp = weatherData.list[0].main.temp;
 var humidity = weatherData.list[0].main.humidity;
 var wind = weatherData.list[0].wind.speed;
@@ -105,9 +111,10 @@ var name = weatherData.city.name
 var iconCode = weatherData.list[0].weather[0].icon;
 var iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
+// Creating an element to display information.
 var card = document.createElement('div');
       card.classList.add('card', 'p-2');
-
+// Organizing the card content with a variable.
 var cardContent = `
           <h2 class="card-title">${name}</h2>
           <img class="weatherIcon" src="${iconUrl}" alt="Weather Icon" />
@@ -115,31 +122,31 @@ var cardContent = `
           <p class="card-text">Humidity: ${humidity}%</p>
           <p class="card-text">Wind: ${wind}</p>
       `;
-
+// Appending information to the Card. 
     card.innerHTML = cardContent;
       currentContainer.appendChild(card);
 
 }
 
-
+// Five-day forecast function.
 function fiveResults(weatherData) {
   var fiveDayContainer = document.querySelector('.five-day');
   fiveDayContainer.innerHTML = ''; 
-  
+  // Designating the amount of days with a for-loop.
   for (var i = 1; i < 6; i++) {
       var card = document.createElement('div');
       card.classList.add('card', 'p-2');
-
+// Displaying the date as mm/dd for each day.
       var date = new Date(weatherData.list[i * 7].dt * 1000);
       var dateString = (date.getMonth() + 1).toString().padStart(1, '0') + '/' + date.getDate().toString().padStart(1, '0');
-
+// Variables displaying in the days from the API.
       var temp = weatherData.list[i * 7].main.temp;
       var humidity = weatherData.list[i * 7].main.humidity;
       var wind = weatherData.list[i * 7].wind.speed;
       var iconCode = weatherData.list[i * 7].weather[0].icon;
 
       var iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
-
+// Organizing card content.
       var cardContent = `
           <h5 class="card-title">${dateString}</h5>
           <img src="${iconUrl}" alt="Weather Icon" />
@@ -147,13 +154,13 @@ function fiveResults(weatherData) {
           <p class="card-text">Humidity: ${humidity}%</p>
           <p class="card-text">Wind: ${wind}</p>
       `;
-
+// Appeniding information to five cards for each day. 
       card.innerHTML = cardContent;
       fiveDayContainer.appendChild(card);
   }
 }
 
-
+// Search History Buttons from local storage. 
 function loadSearchHistory() {
     var searchHistoryContainer = document.querySelector('.searchHistory');
     searchHistoryContainer.innerHTML = '';
@@ -172,12 +179,12 @@ function loadSearchHistory() {
             var clickedSearch = this.textContent;
             searchApi(clickedSearch);
         });
-
+// Displaying each city already search as a button to click that will start the function back at the searchAPI() function.
         searchHistoryContainer.appendChild(button);
         
     }
 }
-
+// Clear history button to delete searched cites from local storage.
   var clearBtn = document.querySelector('.clear')
   var searchList = document.querySelector('.searchHistory')
   clearBtn.addEventListener('click', function clearLocalStorage() {
---
+++ README.md
@@ -0,0 +1,67 @@
+## Weather Dashboard
+
+# User Story
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+# Acceptance Criteria
+
+GIVEN a weather dashboard with form inputs:
+
+- WHEN I search for a city THEN I am presented with current and future conditions for that city and that city is added to the search history
+- WHEN I view current weather conditions for that city THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+- WHEN I view future weather conditions for that city THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+- WHEN I click on a city in the search history THEN I am again presented with current and future conditions for that city
+
+
+## Wireframing
+- The weather dashboard will have an input box where the user can search for different cities as well as a clear history button. The data will display as current weather and a 5-day forecast. Once the search is completed, the city that the user put in the input will display in the search history.
+
+<img src="./Assets/images/weather-dashboard-WF.png" alt="WF for dashboard" width="300" height="auto">
+
+## HTML
+Basic HTML was written to give the page basic elements such as the headers, search card, current weather card, five-day card and search history card. I used the bootstrap grid system to aligm the items within the html. 
+
+## CSS
+Minimal CSS was used for this page. I tried to do as much of the styling through the HTML and javascript. 
+
+## Javascript
+Psuedocoded for btn, searchApi, printResults, fiveResults, loadSearchHistory and clearLocalStorage.
+
+# Btn Function
+    This function uses an event listener to start everything. This function in particular stores the value in localStorage, gives an error prompt for an invalid search and pulls the input value into the next function.
+
+# searchAPI Function
+    This function takes the value from the input and pulls information from an API. The input is first run through an API to get the latitude and longitude by name. This API is called the geocode API from openweather.
+    Once the latitude and longitude are saved as variable, the value of those variables are saved into the URL to fetch information from another API to get all weather related information.
+
+# printResults and fiveResults Functions
+    These functions both take the weatherData from the searchAPI function to display the designated information. The information locations chosen to display are saved as variables and console.logged to confirm the correct information is being pulled from the API. 
+    The printResults function displays the current weather. The fiveResults function runs a for-loop and displays 5 days of weather forecast. 
+
+# loadSearchHistory and clearLocalStorage Functions
+    The loadSearchHistory function pulls the inputs from the local storage and displays them as buttons. Each button has an event listener to run the text content through the searchAPI function.
+    The clearLocalStorage function cleans up the page and deletes the local storage of stored inputs.
+
+## Finished Screenshot
+
+<img src="./Assets/images/weather-dashboard.png" alt="Empty search page" width="300" height="auto">
+<img src="./Assets/images/weather-search.png" alt="search page showing results" width="300" height="auto">
+
+## Installation
+
+- Clone this repository
+
+* $ git clone git@github.com:schneidsmc/weather-dashboard06.git
+
+## Usage
+
+-Deployed URL
+
+* https://schneidsmc.github.io/weather-dashboard06/
+
+## Author
+
+* [schneidsmc](https://github.com/schneidsmc)
+
